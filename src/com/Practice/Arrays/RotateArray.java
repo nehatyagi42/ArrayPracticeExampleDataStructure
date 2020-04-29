ffb10bcc3cfe3f9f8712165283d508391fd7ae88
neha <nehatyagi2516@gmail.com>
2020-04-29 12:30:36 +0530

    First commit

---
+++ src/com/Practice/Arrays/RotateArray.java
@@ -0,0 +1,46 @@
+package com.Practice.Arrays;
+
+//Program for array rotation:
+/*To rotate by one, store arr[0] in a temporary variable temp, move arr[1] to arr[0], arr[2] to arr[1] …and finally temp to arr[n-1]
+
+Let us take the same example arr[] = [1, 2, 3, 4, 5, 6, 7], d = 2
+Rotate arr[] by one 2 times
+We get [2, 3, 4, 5, 6, 7, 1] after first rotation and [ 3, 4, 5, 6, 7, 1, 2] after second rotation.
+
+Below is the implementation of the above approach :*/
+
+
+
+public class RotateArray {
+	/*Function to left rotate arr[] of size n by d*/
+    void leftRotate(int arr[], int d, int n) 
+    { 
+        for (int i = 0; i < d; i++) 
+            leftRotatebyOne(arr, n); 
+    } 
+                           
+    void leftRotatebyOne(int arr[], int n) 
+    { 
+        int i, temp; 
+        temp = arr[0]; 
+        for (i = 0; i < n - 1; i++) 
+            arr[i] = arr[i + 1]; 
+        arr[i] = temp; 
+    } 
+  
+    /* utility function to print an array */
+    void printArray(int arr[], int n) 
+    { 
+        for (int i = 0; i < n; i++) 
+            System.out.print(arr[i] + " "); 
+    } 
+  
+    // Driver program to test above functions 
+    public static void main(String[] args) 
+    { 
+        RotateArray rotate = new RotateArray(); 
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 }; 
+        rotate.leftRotate(arr, 2, 7); 
+        rotate.printArray(arr, 7); 
+    } 
+}
